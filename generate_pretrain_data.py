ab046c46495627e7cb696749ea9af17d20f79516
Mark Van der Merwe <markvdm@umich.edu>
2022-12-05 17:03:16 -0500

    Add shift to pretrained data.

--- generate_pretrain_data.py
+++ generate_pretrain_data.py
@@ -2,21 +2,26 @@ import argparse
 
 import mmint_utils
 import open3d as o3d
+import vedo_utils
 import utils
-from vedo import Plotter, Points, Arrows
+from vedo import Plotter, Points
 
 
 # TODO: Unified visualization toolkit.
 def vis_sdf_data(query_points, sdf):
     plt = Plotter(shape=(1, 2))
-    plt.at(0).show(Points(query_points), utils.draw_axes(), "All Sample Points")
-    plt.at(1).show(Points(query_points[sdf <= 0.0], c="b"), utils.draw_axes(), "Occupied Points")
+    plt.at(0).show(Points(query_points), vedo_utils.draw_origin(), "All Sample Points")
+    plt.at(1).show(Points(query_points[sdf <= 0.0], c="b"), vedo_utils.draw_origin(), "Occupied Points")
     plt.interactive().close()
 
 
 def generate_pretrain_data(mesh_fn: str, out_fn: str, n: int = 50000, vis: bool = False):
     mesh = o3d.io.read_triangle_mesh(mesh_fn)
-    query_points, sdf = utils.get_sdf_values(mesh, n=n)
+    query_points, sdf = utils.get_sdf_values(mesh, n_random=n // 2, n_off_surface=n // 2)
+
+    # Add mount height to query points to account for the tool mounting.
+    mount_height = 0.036
+    query_points[:, 2] += mount_height
 
     data_dict = {
         "n_points": n,
